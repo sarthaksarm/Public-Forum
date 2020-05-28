d16a7f1dd0fe977310c3e5fffa686be65c424c72
sarthaksarm <sarmsarthak@gmail.com>
2020-05-28 23:41:05 +0530

    MsgAdapter, Migration to androidx

--- Public_Forum/app/src/main/java/com/sarm/publicforum/ChatMain.java
+++ Public_Forum/app/src/main/java/com/sarm/publicforum/ChatMain.java
@@ -2,9 +2,6 @@ package com.sarm.publicforum;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,10 +10,14 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
